1cdfbf0c95dd192d7ce40082efcaea5055c5e617
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2020-05-20 08:11:16 -0500

    Initial commit

---
+++ src/main/java/com/alaythiaproductions/deltaserving/DeltaServingApplication.java
@@ -0,0 +1,13 @@
+package com.alaythiaproductions.deltaserving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaServingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaServingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alaythiaproductions/deltaserving/DeltaServingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaythiaproductions.deltaserving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaServingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
